219ec599aef48b3e18aff5a26c709c272c9fb645
ConMcg2025 <cjmcgrail2011@hotmail.com>
2026-04-29 12:51:41 +0200

    Update AdminController.java

--- src/main/java/dk/easv/weblagerexam/gui/AdminController.java
+++ src/main/java/dk/easv/weblagerexam/gui/AdminController.java
@@ -1,4 +1,71 @@
 package dk.easv.weblagerexam.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AdminController {
+
+    @FXML
+    private Button addUserBtn;
+
+    @FXML
+    private Button deleteUserBtn;
+
+    @FXML
+    private Button editUserBtn;
+
+    @FXML
+    private Button fileInfoButton;
+
+    @FXML
+    private Button historyButton;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private TableView<?> mainTable;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TableColumn<?, ?> typeColumn;
+
+    @FXML
+    private Button usersButton;
+
+    @FXML
+    void onAddUserBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteUserBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEditUserBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onFileInfoClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHistoryClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUsersClicked(ActionEvent event) {
+
+    }
+
 }
